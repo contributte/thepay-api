fffd4d162423739bc0801a32163b5a38cc8ce5dd
Jan Trejbal <jan@trejbal.land>
2021-12-27 22:19:40 +0100

    Add missing return type to the DataApiObject::offsetExists

--- src/DataApi/DataApiObject.php
+++ src/DataApi/DataApiObject.php
@@ -118,7 +118,7 @@ abstract class DataApiObject implements ArrayAccess
 	 * @param string $offset
 	 * @return bool
 	 */
-	public function offsetExists($offset)
+	public function offsetExists($offset): bool
 	{
 		$keys = static::keys();
 		return in_array($offset, $keys, true);
